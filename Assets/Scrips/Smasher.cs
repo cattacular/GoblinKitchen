6e9e25472882b6fbe9dea3027db18d25ab1f9d64
Chase Prochnow <46829087+cprochnow28@users.noreply.github.com>
2022-03-29 22:12:38 -0500

    added tomato, mallet and made it smashable as prefabs also fixed knife

---
+++ Assets/Scrips/Smasher.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smasher : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.GetComponent<Smashable>()){
+            other.gameObject.GetComponent<Collider>().enabled = false;
+            Smashable original = other.gameObject.GetComponent<Smashable>();
+            Instantiate(original.smashed, other.gameObject.transform.position, other.gameObject.transform.rotation);
+            Destroy(other.gameObject);
+        }
+    }
+}
