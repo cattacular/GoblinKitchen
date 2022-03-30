ed019c48c59ac1c6c9e3672d712c16227e0577f1
Chase Prochnow <46829087+cprochnow28@users.noreply.github.com>
2022-03-30 17:21:17 -0500

    fixed some prefabs and built a milestone 2 scene

--- Assets/Scrips/Cutter.cs
+++ Assets/Scrips/Cutter.cs
@@ -6,11 +6,17 @@ public class Cutter : MonoBehaviour
 {
     void OnTriggerEnter(Collider other){
         if(other.gameObject.GetComponent<Cuttable>()){
+            this.gameObject.GetComponent<Collider>().isTrigger = false;
             other.gameObject.GetComponent<Collider>().enabled = false;
             Cuttable original = other.gameObject.GetComponent<Cuttable>();
             Instantiate(original.leftHalf, other.gameObject.transform.position, other.gameObject.transform.rotation);
             Instantiate(original.rightHalf, other.gameObject.transform.position, other.gameObject.transform.rotation);
             Destroy(other.gameObject);
+            Invoke("makeCuttable", 5);
         }
+    }
+
+    void makeCuttable(){
+        this.gameObject.GetComponent<Collider>().isTrigger = true;
     }
 }
