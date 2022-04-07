b9bbf33a7d6b3b4ecef8f9b9dd0905d1f49af443
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2022-04-07 17:23:51 -0500

    Added acceleration and fog in my scene

--- Assets/Scrips/Climber.cs
+++ Assets/Scrips/Climber.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
@@ -9,13 +10,24 @@ public class Climber : MonoBehaviour
     private CharacterController character;
     public static XRController climbingHand;
     private ContinuousMovement continuousMovement;
+    private bool Accelerator;
+    private float tempVelocityx;
+    private float tempVelocityy;
+    private float tempVelocityz;
+    private float inversetempVelocityx;
+    private float inversetempVelocityy;
+    private float inversetempVelocityz;
+
+    private float accelerationFactor;
     
 
     // Start is called before the first frame update
     void Start()
     {
+        accelerationFactor = .03f;
         character = GetComponent<CharacterController>();
         continuousMovement = GetComponent<ContinuousMovement>();
+        Accelerator = false;
     }
 
     // Update is called once per frame
@@ -25,9 +37,16 @@ public class Climber : MonoBehaviour
         {
             continuousMovement.enabled = false;
             Climb();
+            Accelerator = true;
         }
-        else
+        else if (Accelerator)
         {
+            if ((inversetempVelocityx > 0 && (tempVelocityx < 0)) || (inversetempVelocityx < 0 && (tempVelocityx > 0)) || (inversetempVelocityy > 0 && (tempVelocityy < inversetempVelocityy)) || (inversetempVelocityy < 0 && (tempVelocityy > inversetempVelocityy)) || (inversetempVelocityz > 0 && (tempVelocityz < 0)) || (inversetempVelocityz < 0 && (tempVelocityz > 0)) ){
+                Accelerate();
+            }else{
+                Accelerator = false;
+            }
+        } else {
             continuousMovement.enabled = true;
         }
     }
@@ -35,9 +54,31 @@ public class Climber : MonoBehaviour
     void Climb()
     {
         InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+        tempVelocityx = velocity.x;
+        tempVelocityy = -velocity.y;
+        tempVelocityz = velocity.z;
+
+        inversetempVelocityx = -velocity.x;
+        inversetempVelocityy = velocity.y;
+        inversetempVelocityz = -velocity.z;
 
         //character.Move(new Vector3(0,-velocity.y * Time.fixedDeltaTime, 0)); This is just for climbing in the y direction
         //character.Move(transform.rotation * -velocity * Time.fixedDeltaTime); this is how the tutorial said to put in climbing
-        character.Move(transform.rotation * new Vector3(velocity.x, -velocity.y, velocity.z) * Time.fixedDeltaTime); // Implemented it this way because having the diections reversed for left to right and forward and backwards is wierd
+        character.Move(transform.rotation * new Vector3(tempVelocityx, tempVelocityy, tempVelocityz) * .01f); // Implemented it this way because having the diections reversed for left to right and forward and backwards is wierd
+    
+    }
+
+    void Accelerate()
+    {
+        if (tempVelocityx * inversetempVelocityx >= 0){
+            tempVelocityx = 0;
+        }
+        if (tempVelocityz * inversetempVelocityz >= 0){
+            tempVelocityz = 0;
+        }
+        character.Move(transform.rotation * new Vector3(tempVelocityx, tempVelocityy*1.5f, tempVelocityz) * .02f);
+        tempVelocityx = (inversetempVelocityx < 0) ?  tempVelocityx - accelerationFactor : tempVelocityx + accelerationFactor;
+        tempVelocityy = (inversetempVelocityy < 0) ? tempVelocityy - accelerationFactor : tempVelocityy + accelerationFactor;
+        tempVelocityz = (inversetempVelocityz < 0) ? tempVelocityz - accelerationFactor : tempVelocityz + accelerationFactor;
     }
 }
